3bd6597c9f0fb8962ef5db7f5934e78890a54634
Alice <Alice@localhost.localdomain>
2018-06-03 00:15:55 +0900

    commit 3

--- homework4.java
+++ homework4.java
@@ -2,5 +2,6 @@ public class homework4{
   public static void main(String args[]){
     System.out.println("Write for commit 1");
     System.out.println("Write for commit 2");
+    System.out.println("Write for commit 3");
   }
 }
